9051daf878918bd69491a7fa6c0d50914ebdd7e2
AlexAdutov <alexeyspark@yandex.ru>
2023-08-27 10:18:35 +0300

    Task 1 2 3

---
+++ event-object/tabs/task.js
@@ -0,0 +1,19 @@
+const tabs = Array.from(document.querySelectorAll(".tab"));
+const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
+//console.log(tabs);
+//console.log(tabsContent);
+
+const openMenu = function (event) {
+    let index = tabs.findIndex((tab) => tab.classList.contains("tab_active"));
+    //console.log(index)
+    tabs[index].classList.remove("tab_active");
+    tabsContent[index].classList.remove("tab__content_active");
+    event.currentTarget.classList.toggle("tab_active");
+    index = tabs.findIndex((tab) => tab.classList.contains("tab_active"));
+    //console.log(index)
+    tabsContent[index].classList.toggle("tab__content_active");
+};
+
+tabs.forEach((tab) => tab.addEventListener("click", openMenu));
+
+//setTimeout(openMenu(),5000);
